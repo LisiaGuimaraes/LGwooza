35b5cf9d9fca5dcdbebfa7eb63933af1d8454a71
LisiaGuimaraes <69821042+LisiaGuimaraes@users.noreply.github.com>
2020-12-30 20:47:30 -0300

    Add files via upload

--- ComparaPlanosPage.java
+++ ComparaPlanosPage.java
@@ -2,7 +2,6 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 
 public class ComparaPlanosPage extends BasePage {
 
@@ -10,8 +9,22 @@ public class ComparaPlanosPage extends BasePage {
         super(navegador);
 
     }
+    public ComparaPlanosPage selecionarPlanos() {
 
-    public ComparaPlanosPage selecionaTodosPlanos() {
+        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+        navegador.findElement(By.xpath("//span[text()='oi']")).click();
+
+        return this;
+    }
+    public ComparaPlanosPage desmarcarSelecaoPlanos() {
+
+        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+        navegador.findElement(By.xpath("//span[text()='oi']")).click();
+
+        return this;
+    }
+
+    public ComparaPlanosPage selecionarTodosPlanos() {
 
         navegador.findElement(By.xpath("//span[text()='vivo']")).click();
         navegador.findElement(By.xpath("//span[text()='oi']")).click();
@@ -20,7 +33,7 @@ public class ComparaPlanosPage extends BasePage {
         return this;
     }
 
-    public ComparaPlanosPage desmarcaPlano() {
+    public ComparaPlanosPage desmarcarSelecaoTodosPlano() {
 
         navegador.findElement(By.xpath("//span[text()='claro']")).click();
         navegador.findElement(By.xpath("//span[text()='oi']")).click();
@@ -30,12 +43,109 @@ public class ComparaPlanosPage extends BasePage {
         return this;
     }
 
-    public ComparaPlanosPage selecionaPlanos() {
-
-        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+    public ComparaPlanosPage selecionarTipoPlanosControlePacoteDadosValor(){
+        navegador.findElement(By.xpath("//span[text()='claro']")).click();
         navegador.findElement(By.xpath("//span[text()='oi']")).click();
+        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de 5.0 GB']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de R$ 75,00']")).click();
+        return this;
+    }
+
+    public ComparaPlanosPage desmarcarselecaoTipoPlanosControlePacoteDadosValor(){
+        navegador.findElement(By.xpath("//span[text()='claro']")).click();
+        navegador.findElement(By.xpath("//span[text()='oi']")).click();
+        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de 5.0 GB']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de R$ 75,00']")).click();
+        return this;
+    }
+
+
+
+    public ComparaPlanosPage selecionarTipoPlanoTimControleMais5GB(){
+
+        navegador.findElement(By.xpath("//span[text()='tim']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de 5.0 GB']")).click();
+
+        return this;
+
+    }
+    public ComparaPlanosPage desmarcarSelecaoPlanoTimControle(){
+        navegador.findElement(By.xpath("//span[text()='tim']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de 5.0 GB']")).click();
+
+        return this;
+
+    }
+
+    public ComparaPlanosPage selecionarTipoPlanoTimPosMais5GB(){
+
+        navegador.findElement(By.xpath("//span[text()='tim']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de 5.0 GB']")).click();
+
+        return this;
+
+    }
+    public ComparaPlanosPage desmarcarSelecaoPlanoTimPos(){
+        navegador.findElement(By.xpath("//span[text()='tim']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Mais de 5.0 GB']")).click();
+
+        return this;
+
+    }
+
+    public ComparaPlanosPage selecionarTipoPlanoClaro(){
+        navegador.findElement(By.xpath("//span[text()='claro']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+
+        return this;
+
+    }
+
+    public ComparaPlanosPage desmarcarPlanoClaro(){
+        navegador.findElement(By.xpath("//span[text()='claro']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+
+        return this;
+
+    }
+
+    public ComparaPlanosPage selecionarTipoPlanoVivo(){
+        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+
+        return this;
+
+    }
+
+    public ComparaPlanosPage desmarcarPlanovivo(){
+        navegador.findElement(By.xpath("//span[text()='vivo']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
 
         return this;
     }
+
+    public ComparaPlanosPage selecionarTipoPlanoOi(){
+        navegador.findElement(By.xpath("//span[text()='oi']")).click();
+        navegador.findElement(By.xpath("//span[text()='Pós']")).click();
+        navegador.findElement(By.xpath("//span[text()='Controle']")).click();
+
+        return this;
+    }
+
+
 
 }
